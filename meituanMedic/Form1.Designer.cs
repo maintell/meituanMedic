10974b8953efcf9ad27525e124d92c6f8ccc495f
shaofeng <shaofeng@github.com>
2020-11-28 00:50:14 +0800

    初步实现数据抓取

--- meituanMedic/Form1.Designer.cs
+++ meituanMedic/Form1.Designer.cs
@@ -29,26 +29,96 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FoodName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoldCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoldPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OriginalPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EstimatedPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(946, 40);
+            this.button1.Location = new System.Drawing.Point(1216, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 42);
+            this.button1.Size = new System.Drawing.Size(157, 49);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "抓取";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FoodName,
+            this.SoldCount,
+            this.SoldPrice,
+            this.OriginalPrice,
+            this.EstimatedPrice});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(1196, 735);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // FoodName
+            // 
+            this.FoodName.DataPropertyName = "FoodName";
+            this.FoodName.HeaderText = "商品名称";
+            this.FoodName.MinimumWidth = 6;
+            this.FoodName.Name = "FoodName";
+            this.FoodName.Width = 375;
+            // 
+            // SoldCount
+            // 
+            this.SoldCount.DataPropertyName = "SoldCount";
+            this.SoldCount.HeaderText = "每月销量";
+            this.SoldCount.MinimumWidth = 6;
+            this.SoldCount.Name = "SoldCount";
+            this.SoldCount.Width = 125;
+            // 
+            // SoldPrice
+            // 
+            this.SoldPrice.DataPropertyName = "SoldPrice";
+            this.SoldPrice.HeaderText = "销售价格";
+            this.SoldPrice.MinimumWidth = 6;
+            this.SoldPrice.Name = "SoldPrice";
+            this.SoldPrice.Width = 125;
+            // 
+            // OriginalPrice
+            // 
+            this.OriginalPrice.DataPropertyName = "SoldPrice";
+            this.OriginalPrice.HeaderText = "原始价格";
+            this.OriginalPrice.MinimumWidth = 6;
+            this.OriginalPrice.Name = "OriginalPrice";
+            this.OriginalPrice.Width = 125;
+            // 
+            // EstimatedPrice
+            // 
+            this.EstimatedPrice.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.EstimatedPrice.DataPropertyName = "EstimatedPrice";
+            this.EstimatedPrice.HeaderText = "到手价格";
+            this.EstimatedPrice.MinimumWidth = 6;
+            this.EstimatedPrice.Name = "EstimatedPrice";
+            this.EstimatedPrice.Width = 125;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1080, 641);
+            this.ClientSize = new System.Drawing.Size(1389, 754);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +126,12 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FoodName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoldCount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoldPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OriginalPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EstimatedPrice;
     }
 }
 
--- meituanMedic/Form1.cs
+++ meituanMedic/Form1.cs
@@ -3,80 +3,149 @@ using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Remote;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace meituanMedic
 {
+    struct GoodInfo
+    {
+        /// <summary>
+        /// 商品名称
+        /// </summary>
+        public string FoodName { set; get; }
+        /// <summary>
+        /// 每月销量
+        /// </summary>
+        public int SoldCount { set; get; }
+        /// <summary>
+        /// 销售价格
+        /// </summary>
+        public decimal SoldPrice { set; get; }
+        /// <summary>
+        /// 原始价格
+        /// </summary>
+        public decimal OriginalPrice { set; get; }
+        /// <summary>
+        /// 到手价格
+        /// </summary>
+        public decimal EstimatedPrice { set; get; }
+    }
+
     public partial class Form1 : Form
     {
+        private AndroidDriver<AndroidElement> driver;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private AndroidDriver<AndroidElement> driver;
+        Dictionary<string, GoodInfo> data = new Dictionary<string, GoodInfo>();
 
+        int GetInteger(string str)
+        {
+            str = Regex.Replace(str, @"[^0-9]+", "");
+            return int.Parse(str);
+        }
+
+        decimal GetDecimal(string str)
+        {
+            str = Regex.Replace(str, @"[^\d.\d]", "");
+            // 如果是数字，则转换为decimal类型
+            if (Regex.IsMatch(str, @"^[+-]?\d*[.]?\d*$"))
+            {
+                return decimal.Parse(str);
+            }
+            return new decimal(0);
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //DesiredCapabilities capabilities = new DesiredCapabilities();
-
-            //capabilities.SetCapability("deviceName", "127.0.0.1:7555");
-            ////capabilities.SetCapability("automationName", "Appium");
-
-            ////capabilities.SetCapability("platformName", "Android");
-            ////capabilities.SetCapability("platformVersion", "6.0"); //可以根据自己的模拟器版版本进行修改。
-            //capabilities.SetCapability("appPackage", "com.sankuai.meituan"); //安卓自带计算器
-            //capabilities.SetCapability("appActivity", "com.meituan.android.pt.homepage.activity.MainActivity");
-            //capabilities.SetCapability("platformName", "Android");
-            //capabilities.SetCapability("noReset", "true");
-
-            DriverOptions driverOptions = new AppiumOptions();
-            driverOptions.AddAdditionalCapability("deviceName", "127.0.0.1:7555");
-            driverOptions.AddAdditionalCapability("appPackage", "com.sankuai.meituan"); //安卓自带计算器
-            driverOptions.AddAdditionalCapability("appActivity", "com.meituan.android.pt.homepage.activity.MainActivity");
-            driverOptions.AddAdditionalCapability("platformName", "Android");
-            driverOptions.AddAdditionalCapability("noReset", "true");
-
-
-
-
-            //var driver = new AppiumDriver(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities);
-
-
-            //RemoteWebDriver wd = new RemoteWebDriver(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities,TimeSpan.FromSeconds(60));
-            var androidDriver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), driverOptions, TimeSpan.FromSeconds(60));
-
             try
             {
-                
-
-                var result = androidDriver.FindElementsByAccessibilityId("com.sankuai.meituan:id/tv_stickyfood_sold_count");
-
-                //By byAccessibilityId = new ByAccessibilityId("Graphics");
-                //wd.FindElement(byAccessibilityId).Text
-
-                //var Soldouts = wd.FindElementsById("com.sankuai.meituan:id/tv_stickyfood_sold_count");
-                
-
-                Console.WriteLine(result.Count);
-
+                var recycler = driver.FindElementById("com.sankuai.meituan:id/recycler");
+                var items = recycler.FindElementsById("com.sankuai.meituan:id/ll_stickyfoodList_adapter_food_food");
+                Console.WriteLine(items.Count);
+                foreach (var item in items)
+                {
+                    // FindElementsById 不会抛异常
+                    var foodNames = item.FindElementsById("com.sankuai.meituan:id/txt_stickyfoodList_adapter_food_name");
+                    var soldCounts = item.FindElementsById("com.sankuai.meituan:id/tv_stickyfood_sold_count");
+                    var soldPrices = item.FindElementsById("com.sankuai.meituan:id/txt_stickyfoodList_adapter_food_price_fix");
+                    var originalPrices = item.FindElementsById("com.sankuai.meituan:id/txt_stickyfoodList_adapter_food_original_price_fix");
+                    var estimatedPrices = item.FindElementsById("com.sankuai.meituan:id/tv_goods_estimated_price");
+                    if (foodNames.Count > 0)
+                    {
+                        var foodName = foodNames.FirstOrDefault()?.Text;
+                        var soldCount = soldCounts.FirstOrDefault()?.Text;
+                        var soldPrice = soldPrices.FirstOrDefault()?.Text;
+                        var originalPrice = originalPrices.FirstOrDefault()?.Text;
+                        var estimatedPrice = estimatedPrices.FirstOrDefault()?.Text;
+                        var info = new GoodInfo
+                        {
+                            FoodName = foodName ?? "",
+                            SoldCount = GetInteger(soldCount ?? "0"),
+                            SoldPrice = GetDecimal(soldPrice ?? "0.0"),
+                            OriginalPrice = GetDecimal(originalPrice ?? "0.0"),
+                            EstimatedPrice = GetDecimal(estimatedPrice ?? "0.0")
+                        };
+                        // 没有添加到字典
+                        if (!data.ContainsKey(info.FoodName))
+                        {
+                            data.Add(info.FoodName, info);
+                        }
+                        Console.WriteLine($"{info.FoodName} 销量：{info.SoldCount} 售价：{info.SoldPrice} 原价：{info.OriginalPrice} 到手价：{info.EstimatedPrice}");
+                    }
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            finally { androidDriver.Quit(); }
+            //dataGridView1.AutoGenerateColumns = false;
+            //dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            dataGridView1.DataSource = data.Values.ToArray<GoodInfo>();
+        }
 
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DriverOptions options = new AppiumOptions();
+                options.AddAdditionalCapability("deviceName", "127.0.0.1:7555");
+                options.AddAdditionalCapability("platformName", "Android");
+                options.AddAdditionalCapability("platformVersion", "6.0.1");
+                //driverOptions.AddAdditionalCapability("appPackage", "com.sankuai.meituan");
+                options.AddAdditionalCapability("appActivity", "com.meituan.android.pt.homepage.activity.MainActivity");
+                options.AddAdditionalCapability("noReset", "true");
 
+                driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), options, TimeSpan.FromSeconds(60));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
